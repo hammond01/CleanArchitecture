8db2ba62770d9116b2d75f361ac90b6d78ca4942
Hammond <hieutruonghoang01@gmail.com>
2024-11-03 23:08:28 +0700

    add AuditLog table

--- src/MonolithArchitecture/src/Application/ProductManager.Domain/Entities/AuditLogEntry.cs
+++ src/MonolithArchitecture/src/Application/ProductManager.Domain/Entities/AuditLogEntry.cs
@@ -1,12 +1,12 @@
 ﻿namespace ProductManager.Domain.Entities;
 
-public class AuditLogEntry : Entity<Guid>
+public class AuditLog : Entity<Guid>
 {
     public Guid UserId { get; set; }
 
     public string Action { get; set; } = default!;
 
-    public string ObjectId { get; set; }= default!;
+    public string ObjectId { get; set; } = default!;
 
-    public string Log { get; set; }= default!;
+    public string Log { get; set; } = default!;
 }
