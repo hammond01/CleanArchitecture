52314311d80cd93d2525c11663515633eb518643
Hammond <hieutruonghoang01@gmail.com>
2024-11-03 23:12:01 +0700

    Created object Extension, Current User Login and PassWord Hash

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Domain/Identity/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿using ProductManager.Domain.Entities;
+namespace ProductManager.Domain.Identity;
+
+public interface IPasswordHasher
+{
+    bool VerifyHashedPassword(ApplicationUser user, string hashedPassword, string providedPassword);
+}
