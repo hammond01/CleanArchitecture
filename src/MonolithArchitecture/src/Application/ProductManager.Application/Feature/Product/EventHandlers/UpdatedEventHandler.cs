dda82f511ddca43c49fdd5a559e5a88080179c3d
Hammond <hieutruonghoang01@gmail.com>
2024-11-07 15:06:37 +0700

    added Product CRUD for Application Layer - CQRS - Event Handler

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Application/Feature/Product/EventHandlers/UpdatedEventHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace ProductManager.Application.Feature.Product.EventHandlers;
+
+public class ProductUpdatedEventHandler : IDomainEventHandler<EntityUpdatedEvent<Products>>
+{
+    private readonly ICrudService<AuditLog> _auditService;
+    private readonly ICurrentUser _currentUser;
+    private readonly IRepository<OutboxEvent, string> _outboxEventRepository;
+
+    public ProductUpdatedEventHandler(ICrudService<AuditLog> auditService, ICurrentUser currentUser,
+        IRepository<OutboxEvent, string> outboxEventRepository)
+    {
+        _auditService = auditService;
+        _currentUser = currentUser;
+        _outboxEventRepository = outboxEventRepository;
+    }
+    public async Task HandleAsync(EntityUpdatedEvent<Products> domainEvent, CancellationToken cancellationToken = default)
+    {
+        var auditLog = new AuditLog
+        {
+            Id = UlidExtension.Generate(),
+            UserId = _currentUser.IsAuthenticated ? _currentUser.UserId : string.Empty,
+            CreatedDateTime = domainEvent.EventDateTime,
+            Action = EventTypeConstants.ProductUpdated,
+            ObjectId = domainEvent.Entity.Id,
+            Log = domainEvent.Entity.AsJsonString()
+        };
+
+        await _auditService.AddAsync(auditLog, cancellationToken);
+
+        var outboxEvent = new OutboxEvent
+        {
+            Id = UlidExtension.Generate(),
+            EventType = EventTypeConstants.ProductUpdated,
+            TriggeredById = _currentUser.UserId,
+            CreatedDateTime = domainEvent.EventDateTime,
+            ObjectId = domainEvent.Entity.Id,
+            Message = domainEvent.Entity.AsJsonString(),
+            Published = false
+        };
+
+        await _outboxEventRepository.AddAsync(outboxEvent, cancellationToken);
+        await _outboxEventRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/MonolithArchitecture/src/Application/ProductManager.Application/Feature/Product/Queries/GetById.cs
@@ -0,0 +1,25 @@
+﻿namespace ProductManager.Application.Feature.Product.Queries;
+
+public record GetProductByIdQuery : IQuery<ApiResponse>
+{
+    public GetProductByIdQuery(string categoryId)
+    {
+        ProductId = categoryId;
+    }
+    public string ProductId { get; set; }
+}
+public class GetProductByIdHandler : IQueryHandler<GetProductByIdQuery, ApiResponse>
+{
+    private readonly ICrudService<Products> _crudService;
+    public GetProductByIdHandler(ICrudService<Products> categoryService)
+    {
+        _crudService = categoryService;
+    }
+
+    public async Task<ApiResponse> HandleAsync(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        var categories = await _crudService.GetByIdAsync(request.ProductId, cancellationToken);
+        return categories == null ? new ApiResponse(404, "No data found.")
+            : new ApiResponse(200, "Get data by Id successfully", categories);
+    }
+}
