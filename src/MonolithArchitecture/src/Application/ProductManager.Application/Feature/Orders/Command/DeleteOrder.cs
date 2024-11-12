a00707e9ee9b8464c254aa17bfc84583b7ec6f5c
Hammond <hieutruonghoang01@gmail.com>
2024-11-12 17:03:53 +0700

    Add order command in Application Layer

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Application/Feature/Orders/Command/DeleteOrder.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductManager.Application.Feature.Orders.Command;
+
+public class DeleteOrderCommand : ICommand<ApiResponse>
+{
+    public DeleteOrderCommand(Order suppliers)
+    {
+        Orders = suppliers;
+    }
+
+    public Order Orders { get; set; }
+}
+public class DeleteOrderCommandHandler : ICommandHandler<DeleteOrderCommand, ApiResponse>
+{
+    private readonly ICrudService<Order> _supplierService;
+
+    public DeleteOrderCommandHandler(ICrudService<Order> supplierService)
+    {
+        _supplierService = supplierService;
+    }
+
+    public async Task<ApiResponse> HandleAsync(DeleteOrderCommand request, CancellationToken cancellationToken)
+    {
+        await _supplierService.DeleteAsync(request.Orders, cancellationToken);
+
+        return new ApiResponse
+        {
+            Message = "Order deleted successfully"
+        };
+    }
+}
