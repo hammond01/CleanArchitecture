cdec7c18a42fb16b67a5312092e0cf334493be37
Hammond <hieutruonghoang01@gmail.com>
2024-11-13 23:36:07 +0700

    Order eventhandler Create and Delete

--- src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Constants/EventTypeConstants.cs
+++ src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Constants/EventTypeConstants.cs
@@ -13,4 +13,8 @@ public class EventTypeConstants
     public const string SupplierCreated = "SUPPLIER_CREATED";
     public const string SupplierUpdated = "SUPPLIER_UPDATED";
     public const string SupplierDeleted = "SUPPLIER_DELETED";
+
+    public const string OrderCreated = "ORDER_CREATED";
+    public const string OrderUpdated = "ORDER_UPDATED";
+    public const string OrderDeleted = "ORDER_DELETED";
 }
