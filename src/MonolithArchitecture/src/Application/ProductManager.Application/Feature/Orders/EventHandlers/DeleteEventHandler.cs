472beaf88bff5ddd53bb657b4a1572d22319fedb
Hammond <hieutruonghoang01@gmail.com>
2024-11-13 23:36:07 +0700

    Order eventhandler Create and Delete

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Application/Feature/Orders/EventHandlers/DeleteEventHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace ProductManager.Application.Feature.Orders.EventHandlers;
+
+public class OrderDeleteEventHandler : IDomainEventHandler<EntityDeletedEvent<Order>>
+{
+    private readonly ICrudService<AuditLog> _auditService;
+    private readonly ICurrentUser _currentUser;
+    private readonly IRepository<OutboxEvent, string> _outboxEventRepository;
+
+    public OrderDeleteEventHandler(ICrudService<AuditLog> auditService, ICurrentUser currentUser,
+        IRepository<OutboxEvent, string> outboxEventRepository)
+    {
+        _auditService = auditService;
+        _currentUser = currentUser;
+        _outboxEventRepository = outboxEventRepository;
+    }
+    public async Task HandleAsync(EntityDeletedEvent<Order> domainEvent, CancellationToken cancellationToken = default)
+    {
+        var auditLog = new AuditLog
+        {
+            Id = UlidExtension.Generate(),
+            UserId = _currentUser.IsAuthenticated ? _currentUser.UserId : string.Empty,
+            CreatedDateTime = domainEvent.EventDateTime,
+            Action = EventTypeConstants.OrderDeleted,
+            ObjectId = domainEvent.Entity.Id,
+            Log = domainEvent.Entity.AsJsonString()
+        };
+
+        await _auditService.AddAsync(auditLog, cancellationToken);
+
+        var outboxEvent = new OutboxEvent
+        {
+            Id = UlidExtension.Generate(),
+            EventType = EventTypeConstants.OrderDeleted,
+            TriggeredById = _currentUser.UserId,
+            CreatedDateTime = domainEvent.EventDateTime,
+            ObjectId = domainEvent.Entity.Id,
+            Message = domainEvent.Entity.AsJsonString(),
+            Published = false
+        };
+
+        await _outboxEventRepository.AddAsync(outboxEvent, cancellationToken);
+        await _outboxEventRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+    }
+}
