8b6b10026a9fd5deeaafd08e5fac526a7f2af58d
Hammond <hieutruonghoang01@gmail.com>
2024-11-14 10:03:29 +0700

    Add Order API

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Application/Feature/Orders/Queries/Gets.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductManager.Application.Feature.Orders.Queries;
+
+public record GetOrders : IQuery<ApiResponse>;
+internal class GetsOrderHandler : IQueryHandler<GetOrders, ApiResponse>
+{
+    private readonly ICrudService<Order> _crudService;
+    public GetsOrderHandler(ICrudService<Order> crudService)
+    {
+        _crudService = crudService;
+    }
+    public async Task<ApiResponse> HandleAsync(GetOrders query, CancellationToken cancellationToken = default)
+    {
+        var response = await _crudService.GetAsync(cancellationToken);
+        return new ApiResponse(200, "Get suppliers successfully", response);
+    }
+}
---
+++ src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Shared/DTOs/OrderDto/CreateOrderDto.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductManager.Shared.DTOs.OrderDto;
+
+public class CreateOrderDto
+{
+    public string? CustomerId { get; set; }
+
+    public string? EmployeeId { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public DateTime? RequiredDate { get; set; }
+
+    public DateTime? ShippedDate { get; set; }
+
+    public string? ShipVia { get; set; }
+
+    public decimal? Freight { get; set; }
+
+    public string? ShipName { get; set; }
+
+    public string? ShipAddress { get; set; }
+
+    public string? ShipCity { get; set; }
+
+    public string? ShipRegion { get; set; }
+
+    public string? ShipPostalCode { get; set; }
+
+    public string? ShipCountry { get; set; }
+}
