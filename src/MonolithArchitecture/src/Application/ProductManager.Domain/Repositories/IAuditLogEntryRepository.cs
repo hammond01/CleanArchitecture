023078f8661f8d84ea8ed2c1da418234563880c8
Hammond <hieutruonghoang01@gmail.com>
2024-11-21 22:03:41 +0700

    update audit log

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Domain/Repositories/IAuditLogEntryRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductManager.Domain.Repositories;
+
+public class AuditLogEntryQueryOptions
+{
+    public Guid UserId { get; set; }
+
+    public string ObjectId { get; set; } = string.Empty;
+
+    public bool AsNoTracking { get; set; }
+}
+public interface IAuditLogEntryRepository : IRepository<AuditLog, string>
+{
+    IQueryable<AuditLog> Get(AuditLogEntryQueryOptions queryOptions);
+}
---
+++ src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Shared/DTOs/AuditLogDto/AuditLogEntryDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductManager.Shared.DTOs.AuditLogDto;
+
+public class AuditLogEntryDto
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string UserName { get; set; } = string.Empty;
+
+    public string Action { get; set; } = string.Empty;
+
+    public string ObjectId { get; set; } = string.Empty;
+
+    public string Log { get; set; } = string.Empty;
+
+    public DateTimeOffset CreatedDateTime { get; set; }
+}
