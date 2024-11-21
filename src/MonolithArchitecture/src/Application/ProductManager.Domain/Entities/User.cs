07f26f7aa3ff137a4929a14b238e4120a81872f8
Hammond <hieutruonghoang01@gmail.com>
2024-11-21 22:03:53 +0700

    update new Entities

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Domain/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿namespace ProductManager.Domain.Entities;
+
+public class User : Entity<Guid>
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public string NormalizedUserName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string NormalizedEmail { get; set; } = string.Empty;
+
+    public bool EmailConfirmed { get; set; }
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public bool PhoneNumberConfirmed { get; set; }
+
+    public bool TwoFactorEnabled { get; set; }
+
+    public string ConcurrencyStamp { get; set; } = string.Empty;
+
+    public string SecurityStamp { get; set; } = string.Empty;
+
+    public bool LockoutEnabled { get; set; }
+
+    public DateTimeOffset? LockoutEnd { get; set; }
+
+    public int AccessFailedCount { get; set; }
+
+    public string Auth0UserId { get; set; } = string.Empty;
+
+    public string AzureAdB2CUserId { get; set; } = string.Empty;
+}
