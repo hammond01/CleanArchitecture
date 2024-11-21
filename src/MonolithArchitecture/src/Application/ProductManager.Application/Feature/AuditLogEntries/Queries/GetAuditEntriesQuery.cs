f1d28b943fefe4c13044e198cf3f43a4bf6890f4
Hammond <hieutruonghoang01@gmail.com>
2024-11-21 22:03:41 +0700

    update audit log

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Application/Feature/AuditLogEntries/Queries/GetAuditEntriesQuery.cs
@@ -0,0 +1,37 @@
+﻿using ProductManager.Shared.DTOs.AuditLogDto;
+namespace ProductManager.Application.Feature.AuditLogEntries.Queries;
+
+public class GetAuditEntriesQuery : AuditLogEntryQueryOptions, IQuery<List<AuditLogEntryDto>>
+{
+}
+internal class GetAuditEntriesQueryHandler : IQueryHandler<GetAuditEntriesQuery, List<AuditLogEntryDto>>
+{
+    private readonly IAuditLogEntryRepository _auditLogEntryRepository;
+    private readonly IUserRepository _userRepository;
+
+    public GetAuditEntriesQueryHandler(IAuditLogEntryRepository auditLogEntryRepository, IUserRepository userRepository)
+    {
+        _auditLogEntryRepository = auditLogEntryRepository;
+        _userRepository = userRepository;
+    }
+
+    public async Task<List<AuditLogEntryDto>> HandleAsync(GetAuditEntriesQuery query, CancellationToken cancellationToken = default)
+    {
+        //var auditLogs = _auditLogEntryRepository.Get(query);
+        //var users = _userRepository.GetQueryableSet();
+        await Task.Delay(1000, cancellationToken);
+        // var rs = auditLogs.Join(users, x => x.UserId, y => y.Id,
+        // (x, y) => new AuditLogEntryDto
+        // {
+        //     Id = x.Id,
+        //     UserId = x.UserId,
+        //     Action = x.Action,
+        //     ObjectId = x.ObjectId,
+        //     Log = x.Log,
+        //     CreatedDateTime = x.CreatedDateTime,
+        //     UserName = y.UserName
+        // });
+
+        return [];
+    }
+}
---
+++ src/MonolithArchitecture/src/Application/ProductManager.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductManager.Domain.Repositories;
+
+public class UserQueryOptions
+{
+    public bool IncludePasswordHistories { get; set; }
+    public bool IncludeClaims { get; set; }
+    public bool IncludeUserRoles { get; set; }
+    public bool IncludeRoles { get; set; }
+    public bool IncludeTokens { get; set; }
+    public bool AsNoTracking { get; set; }
+}
+public interface IUserRepository : IRepository<User, Guid>
+{
+    IQueryable<User> Get(UserQueryOptions queryOptions);
+}
