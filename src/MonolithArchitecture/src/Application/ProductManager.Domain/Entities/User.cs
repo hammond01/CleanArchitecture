5479526b0e93a9e1e2db6a51411bfc0c2c429481
Hammond <hieutruonghoang01@gmail.com>
2024-11-22 16:52:03 +0700

    Update Entities

--- src/MonolithArchitecture/src/Application/ProductManager.Domain/Entities/User.cs
+++ src/MonolithArchitecture/src/Application/ProductManager.Domain/Entities/User.cs
@@ -2,26 +2,27 @@
 
 public class User : Entity<Guid>
 {
+    [StringLength(250)]
     public string UserName { get; set; } = string.Empty;
-
+    [StringLength(250)]
     public string NormalizedUserName { get; set; } = string.Empty;
-
+    [StringLength(250)]
     public string Email { get; set; } = string.Empty;
-
+    [StringLength(250)]
     public string NormalizedEmail { get; set; } = string.Empty;
-
+    [StringLength(250)]
     public bool EmailConfirmed { get; set; }
-
+    [StringLength(250)]
     public string PasswordHash { get; set; } = string.Empty;
-
+    [StringLength(250)]
     public string PhoneNumber { get; set; } = string.Empty;
 
     public bool PhoneNumberConfirmed { get; set; }
 
     public bool TwoFactorEnabled { get; set; }
-
+    [StringLength(250)]
     public string ConcurrencyStamp { get; set; } = string.Empty;
-
+    [StringLength(250)]
     public string SecurityStamp { get; set; } = string.Empty;
 
     public bool LockoutEnabled { get; set; }
@@ -29,8 +30,8 @@ public class User : Entity<Guid>
     public DateTimeOffset? LockoutEnd { get; set; }
 
     public int AccessFailedCount { get; set; }
-
+    [StringLength(250)]
     public string Auth0UserId { get; set; } = string.Empty;
-
+    [StringLength(250)]
     public string AzureAdB2CUserId { get; set; } = string.Empty;
 }
