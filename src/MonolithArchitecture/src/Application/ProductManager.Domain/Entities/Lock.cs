b4b70ceaf1703e983ddc9a13bad33a70bd84f29a
Hammond <hieutruonghoang01@gmail.com>
2024-11-22 16:48:13 +0700

    add Entities Lock, Role, RoleClaim

---
+++ src/MonolithArchitecture/src/Application/ProductManager.Domain/Entities/Lock.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductManager.Domain.Entities;
+
+public class Lock
+{
+    public string EntityId { get; set; } = default!;
+
+    public string EntityName { get; set; } = default!;
+
+    public string OwnerId { get; set; } = default!;
+
+    public DateTimeOffset? AcquiredDateTime { get; set; }
+
+    public DateTimeOffset? ExpiredDateTime { get; set; }
+}
---
+++ src/MonolithArchitecture/src/Application/ProductManager.Domain/Entities/RoleClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductManager.Domain.Entities;
+
+public class RoleClaim : Entity<Guid>
+{
+    public string Type { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+
+    public Role Role { get; set; } = null!;
+}
