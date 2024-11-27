8212c7eabf755df4ef1251eee11631b1eb490a65
Hammond <hieutruonghoang01@gmail.com>
2024-11-27 10:54:09 +0700

    Re Update dbcontext and Entities

--- src/MonolithArchitecture/src/Application/ProductManager.Domain/Entities/RoleClaim.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductManager.Domain.Entities;
-
-public class RoleClaim : Entity<Guid>
-{
-    public string Type { get; set; } = string.Empty;
-    public string Value { get; set; } = string.Empty;
-
-    public Role Role { get; set; } = null!;
-}
