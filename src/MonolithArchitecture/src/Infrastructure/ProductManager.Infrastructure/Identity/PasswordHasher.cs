824305aa434c8746b4bc2c6ceb9fd81ba1c23859
Hammond <hieutruonghoang01@gmail.com>
2024-11-28 23:02:05 +0700

    Update db context add PasswordHasher

--- src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/Identity/PasswordHasher.cs
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/Identity/PasswordHasher.cs
@@ -1,8 +1,9 @@
-﻿namespace ProductManager.Infrastructure.Identity;
+﻿using ProductManager.Domain.Entities.Identity;
+namespace ProductManager.Infrastructure.Identity;
 
 public class PasswordHasher : IPasswordHasher
 {
-    public bool VerifyHashedPassword(ApplicationUser user, string hashedPassword, string providedPassword)
-        => new PasswordHasher<ApplicationUser>().VerifyHashedPassword(user, hashedPassword, providedPassword) !=
+    public bool VerifyHashedPassword(User user, string hashedPassword, string providedPassword)
+        => new PasswordHasher<User>().VerifyHashedPassword(user, hashedPassword, providedPassword) !=
            PasswordVerificationResult.Failed;
 }
