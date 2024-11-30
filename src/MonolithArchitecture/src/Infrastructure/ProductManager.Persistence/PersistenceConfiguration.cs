b7617675ff2007fa44b4cf39c2340bef64f4a247
Hammond <hieutruonghoang01@gmail.com>
2024-11-30 10:34:39 +0700

    Update Infas Layer

--- src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/PersistenceConfiguration.cs
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/PersistenceConfiguration.cs
@@ -1,38 +1,15 @@
-﻿namespace ProductManager.Persistence;
+﻿using ProductManager.Domain.Entities.Identity;
+namespace ProductManager.Persistence;
 
 public static class PersistenceConfiguration
 {
-    public static IServiceCollection AddPersistence(this IServiceCollection services, string connectionString,
-        string migrationsAssembly = "")
+    public static IServiceCollection AddPersistence(this IServiceCollection services, string connectionString)
     {
-        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString, sqlServerOptionsAction: sql =>
-            {
-                if (!string.IsNullOrEmpty(migrationsAssembly))
-                {
-                    sql.MigrationsAssembly(migrationsAssembly);
-                }
-            }))
-            .AddDbContextFactory<ApplicationDbContext>((Action<DbContextOptionsBuilder>)null!, ServiceLifetime.Scoped)
-            .AddRepositories();
-        return services;
-    }
-    public static IServiceCollection AddIdentityPersistence(this IServiceCollection services, string connectionString,
-        string migrationsAssembly = "")
-    {
-        services.AddDbContext<ApplicationIdentityDbContext>(options => options.UseSqlServer(connectionString,
-            sqlServerOptionsAction: sql =>
-            {
-                if (!string.IsNullOrEmpty(migrationsAssembly))
-                {
-                    sql.MigrationsAssembly(migrationsAssembly);
-                }
-            }))
-            .AddDbContextFactory<ApplicationIdentityDbContext>((Action<DbContextOptionsBuilder>)null!, ServiceLifetime.Scoped);
-
-        services.AddDbContext<ApplicationIdentityDbContext>(options =>
+        services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(connectionString));
-        services.AddIdentity<ApplicationUser, IdentityRole>()
-            .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+
+        services.AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
 
         services.AddScoped<IIdentityRepository, IdentityRepository>();
@@ -41,8 +18,10 @@ public static class PersistenceConfiguration
         services.AddScoped<IdentityExtension>();
         services.AddTransient<IDatabaseInitializer, DatabaseInitializer>();
         services.AddSingleton<EntityPermissions>();
+
         return services;
     }
+
     private static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
