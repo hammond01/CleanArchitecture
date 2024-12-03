1275a3500288e8460234b892bc8a866668fd920e
Hammond <hieutruonghoang01@gmail.com>
2024-12-03 10:35:00 +0700

    Init UI blazor

---
+++ src/MonolithArchitecture/src/Presentation/UIs/ProductManager.Blazor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.SignalR;
+using ProductManager.Blazor.Components;
+using ProductManager.Blazor.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddBootstrapBlazor();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddTableDemoDataService();
+
+builder.Services.Configure<HubOptions>(option => option.MaximumReceiveMessageSize = null);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseResponseCompression();
+}
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
