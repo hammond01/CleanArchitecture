8110647e3edcc3f821714d3647deef1cee0b6cb4
Hammond <hieutruonghoang01@gmail.com>
2024-12-04 23:29:58 +0700

    Add Lock Coppy right from PhongNguyen

---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/LockConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace ProductManager.Persistence.MappingConfigurations;
+
+public class LockConfiguration : IEntityTypeConfiguration<Lock>
+{
+    public void Configure(EntityTypeBuilder<Lock> builder)
+    {
+        builder.ToTable("Locks");
+        builder.HasKey(x => new
+        {
+            x.EntityId, x.EntityName
+        });
+        builder.HasIndex(x => new
+        {
+            x.OwnerId
+        });
+    }
+}
