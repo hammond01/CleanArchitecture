36068d1be9067b196fe64cd1aaa5b106fddf53fe
Hammond <hieutruonghoang01@gmail.com>
2024-12-09 16:38:25 +0700

    Sorted

--- src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Constants/ApiResponseConstants/IdentityMessage.cs
+++ src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Constants/ApiResponseConstants/IdentityMessage.cs
@@ -2,18 +2,18 @@
 
 public static class IdentityMessage
 {
-    public static string RequiresTwoFactor => "Two factor authentication required";
+    public static string EmailVerificationSuccessful => "Email verification successful";
+    public static string InvalidRefreshToken => "Invalid refresh token";
     public static string IsLockedOut => "User is locked out";
     public static string IsNotAllowed => "Email not confirmed";
     public static string LoginFailed => "Login failed";
     public static string LoginSuccess => "Login success";
-    public static string InvalidRefreshToken => "Invalid refresh token";
-    public static string TokenRefreshed => "Token refreshed";
     public static string LogoutSuccess => "Logout success";
-    public static string UserNameAndPassRequired => "Username and password are required.";
-    public static string UserAlreadyExists => "User already exists";
-    public static string RegisteredSuccess => "User registered successfully.";
     public static string RegisteredFailed => "User registration failed: ";
-    public static string EmailVerificationSuccessful => "Email verification successful";
+    public static string RegisteredSuccess => "User registered successfully.";
+    public static string RequiresTwoFactor => "Two factor authentication required";
+    public static string TokenRefreshed => "Token refreshed";
+    public static string UserAlreadyExists => "User already exists";
     public static string UserDoesNotExist => "The user doesn't exist.";
+    public static string UserNameAndPassRequired => "Username and password are required.";
 }
