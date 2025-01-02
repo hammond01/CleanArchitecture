60844ac4a941bc04b13f2ff7c75af06b674d5f92
Hammond <hieutruonghoang01@gmail.com>
2025-01-02 14:57:15 +0700

    Added RegionConfiguration

--- src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/TerritoryConfiguration.cs
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/TerritoryConfiguration.cs
@@ -11,25 +11,5 @@ public class TerritoryConfiguration : IEntityTypeConfiguration<Territory>
         builder.HasOne(x => x.Region)
             .WithMany(x => x.Territories)
             .HasForeignKey(x => x.RegionId);
-
-        builder.HasData(new List<Territory>
-        {
-            new Territory
-            {
-                Id = UlidExtension.Generate(), TerritoryDescription = "North", RegionId = "1"
-            },
-            new Territory
-            {
-                Id = UlidExtension.Generate(), TerritoryDescription = "South", RegionId = "1"
-            },
-            new Territory
-            {
-                Id = UlidExtension.Generate(), TerritoryDescription = "East", RegionId = "2"
-            },
-            new Territory
-            {
-                Id = UlidExtension.Generate(), TerritoryDescription = "West", RegionId = "2"
-            }
-        });
     }
 }
