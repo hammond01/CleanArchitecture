30e6fe212fd5a6ef0878e0ae9bc3ab934f5c4a56
Hammond <hieutruonghoang01@gmail.com>
2025-01-02 14:57:15 +0700

    Added RegionConfiguration

---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/RegionConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace ProductManager.Persistence.MappingConfigurations;
+
+public class RegionConfiguration : IEntityTypeConfiguration<Region>
+{
+
+    public void Configure(EntityTypeBuilder<Region> builder)
+    {
+        builder.ToTable("Region");
+        builder.HasKey(x => x.Id);
+
+        builder.HasData(new List<Region>
+        {
+            new Region
+            {
+                Id = UlidExtension.Generate(), RegionDescription = "Ha Noi"
+            },
+            new Region
+            {
+                Id = UlidExtension.Generate(), RegionDescription = "Ho Chi Minh City"
+            },
+            new Region
+            {
+                Id = UlidExtension.Generate(), RegionDescription = "Can Tho"
+            },
+            new Region
+            {
+                Id = UlidExtension.Generate(), RegionDescription = "Nha Trang"
+            }
+        });
+    }
+}
