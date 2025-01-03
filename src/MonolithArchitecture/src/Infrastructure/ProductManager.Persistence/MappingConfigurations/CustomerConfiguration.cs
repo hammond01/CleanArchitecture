50f2a3b79e69a89c6c9b8d81e4a797e8faed0eaf
Hammond <hieutruonghoang01@gmail.com>
2025-01-03 10:20:05 +0700

    added CustomerConfiguration

---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/CustomerConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductManager.Persistence.MappingConfigurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.ToTable("Customers");
+        builder.HasKey(x => x.Id);
+    }
+}
