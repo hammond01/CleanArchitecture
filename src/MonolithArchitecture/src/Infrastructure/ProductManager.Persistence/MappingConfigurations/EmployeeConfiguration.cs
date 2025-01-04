0c54b1698b9bbab6257615b17c55cecf25eb12b8
Hammond <hieutruonghoang01@gmail.com>
2025-01-04 10:36:10 +0700

    Added EmployeeConfiguration

---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/EmployeeConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductManager.Persistence.MappingConfigurations;
+
+public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.ToTable("Employees");
+        builder.HasKey(x => x.Id);
+    }
+}
