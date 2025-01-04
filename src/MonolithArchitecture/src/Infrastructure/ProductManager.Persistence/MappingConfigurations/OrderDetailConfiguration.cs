361495c1bbf0b14d7eb726d64e52bfb9542b62cf
Hammond <hieutruonghoang01@gmail.com>
2025-01-04 11:35:11 +0700

    Added Order and OrderDetail Configuration

---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/OrderDetailConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace ProductManager.Persistence.MappingConfigurations;
+
+public class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
+{
+
+    public void Configure(EntityTypeBuilder<OrderDetail> builder)
+    {
+        builder.ToTable("OrderDetails");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.UnitPrice).HasColumnType("money");
+        builder.Property(x => x.Quantity).HasColumnType("smallint");
+        builder.Property(x => x.Discount).HasColumnType("real");
+        builder.HasOne(x => x.Order)
+            .WithMany(x => x.OrderDetails)
+            .HasForeignKey(x => x.Id);
+        builder.HasOne(x => x.Products)
+            .WithMany(x => x.OrderDetails)
+            .HasForeignKey(x => x.Id);
+    }
+}
