18086bef7322c675a65f4e9e1196e8398b90a394
Hammond <hieutruonghoang01@gmail.com>
2025-01-05 06:39:02 +0700

    Added Product and EmployeeTerritories Configuration

---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/EmployeeTerritoriesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductManager.Persistence.MappingConfigurations;
+
+public class EmployeeTerritoriesConfiguration : IEntityTypeConfiguration<EmployeeTerritory>
+{
+    public void Configure(EntityTypeBuilder<EmployeeTerritory> builder)
+    {
+        builder.ToTable("EmployeeTerritories");
+        builder.HasKey(x => new
+        {
+            x.EmployeeId, x.TerritoryId
+        });
+        builder.HasOne(et => et.Employee)
+            .WithMany(e => e.EmployeeTerritories)
+            .HasForeignKey(et => et.EmployeeId);
+        builder.HasOne(et => et.Territory)
+            .WithMany(t => t.EmployeeTerritories)
+            .HasForeignKey(et => et.TerritoryId);
+    }
+}
