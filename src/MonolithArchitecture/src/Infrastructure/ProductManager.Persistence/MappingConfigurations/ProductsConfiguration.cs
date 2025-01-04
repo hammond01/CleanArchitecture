db699bb5476724deed3e919021dd9f8fb1212a64
Hammond <hieutruonghoang01@gmail.com>
2025-01-05 06:39:02 +0700

    Added Product and EmployeeTerritories Configuration

---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/ProductsConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductManager.Persistence.MappingConfigurations;
+
+public class ProductsConfiguration : IEntityTypeConfiguration<Products>
+{
+
+    public void Configure(EntityTypeBuilder<Products> builder)
+    {
+        builder.ToTable("Products");
+        builder.HasKey(x => x.Id);
+    }
+}
