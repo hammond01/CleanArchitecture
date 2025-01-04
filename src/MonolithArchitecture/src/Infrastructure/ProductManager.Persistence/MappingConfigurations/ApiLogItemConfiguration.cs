12b695b95408879da0a10fd4b313bc61d8b2e218
Hammond <hieutruonghoang01@gmail.com>
2025-01-05 06:16:46 +0700

    Added ApiLogConfiguration

---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Persistence/MappingConfigurations/ApiLogItemConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductManager.Persistence.MappingConfigurations;
+
+public class ApiLogItemConfiguration : IEntityTypeConfiguration<ApiLogItem>
+{
+
+    public void Configure(EntityTypeBuilder<ApiLogItem> builder)
+    {
+        builder.ToTable("ApiLogs");
+        builder.HasKey(x => x.Id);
+    }
+}
