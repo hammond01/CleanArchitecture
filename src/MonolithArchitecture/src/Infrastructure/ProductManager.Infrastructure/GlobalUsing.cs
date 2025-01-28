5b21607b801ebb7b64ee0cad03d210f55f042eea
Hammond <hieutruonghoang01@gmail.com>
2025-01-28 23:54:44 +0700

    Global using and Register PasswordHasher

--- src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/GlobalUsing.cs
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/GlobalUsing.cs
@@ -6,3 +6,4 @@ global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Identity;
 global using ProductManager.Infrastructure.DateTimes;
 global using ProductManager.Infrastructure.Identity;
+global using ProductManager.Domain.Entities.Identity;
--- src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/InfrastructureConfiguration.cs
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/InfrastructureConfiguration.cs
@@ -5,6 +5,7 @@ public static class InfrastructureConfiguration
     public static IServiceCollection InfrastructureConfigureServices(this IServiceCollection services)
     {
         services.AddScoped<ICurrentUser, CurrentWebUser>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
         services.AddDateTimeProvider();
         return services;
     }
