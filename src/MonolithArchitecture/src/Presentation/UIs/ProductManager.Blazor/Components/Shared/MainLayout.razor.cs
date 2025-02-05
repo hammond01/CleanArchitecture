e61950f7179f4ae097ffbe02067d785865645bc2
Hammond <hieutruonghoang01@gmail.com>
2025-02-05 11:30:37 +0700

    Added Blazor UI Login Form

--- src/MonolithArchitecture/src/Presentation/UIs/ProductManager.Blazor/Components/Shared/MainLayout.razor.cs
+++ src/MonolithArchitecture/src/Presentation/UIs/ProductManager.Blazor/Components/Shared/MainLayout.razor.cs
@@ -1,52 +1,68 @@
-﻿using BootstrapBlazor.Components;
-using Microsoft.AspNetCore.Components.Routing;
+﻿namespace ProductManager.Blazor.Components.Shared;
 
-namespace ProductManager.Blazor.Components.Shared
+/// <summary>
+/// </summary>
+public sealed partial class MainLayout
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public sealed partial class MainLayout
+    private bool UseTabSet { get; set; } = true;
+
+    private string Theme
     {
-        private bool UseTabSet { get; set; } = true;
+        get;
+    } = "";
 
-        private string Theme { get; set; } = "";
+    private bool IsOpen { get; set; }
 
-        private bool IsOpen { get; set; }
+    private bool IsFixedHeader { get; set; } = true;
 
-        private bool IsFixedHeader { get; set; } = true;
+    private bool IsFixedFooter { get; set; } = true;
 
-        private bool IsFixedFooter { get; set; } = true;
+    private bool IsFullSide { get; set; } = true;
 
-        private bool IsFullSide { get; set; } = true;
+    private bool ShowFooter { get; set; } = true;
 
-        private bool ShowFooter { get; set; } = true;
+    private List<MenuItem>? Menus { get; set; }
 
-        private List<MenuItem>? Menus { get; set; }
+    /// <summary>
+    ///     OnInitialized 方法
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
 
-        /// <summary>
-        /// OnInitialized 方法
-        /// </summary>
-        protected override void OnInitialized()
+        Menus = GetIconSideMenuItems();
+    }
+
+    private static List<MenuItem> GetIconSideMenuItems()
+    {
+        var menus = new List<MenuItem>
         {
-            base.OnInitialized();
-
-            Menus = GetIconSideMenuItems();
-        }
-
-        private static List<MenuItem> GetIconSideMenuItems()
-        {
-            var menus = new List<MenuItem>
+            new MenuItem
             {
-                new() { Text = "返回组件库", Icon = "fa-solid fa-fw fa-home", Url = "https://www.blazor.zone/components" },
-                new() { Text = "Index", Icon = "fa-solid fa-fw fa-flag", Url = "/" , Match = NavLinkMatch.All},
-                new() { Text = "Counter", Icon = "fa-solid fa-fw fa-check-square", Url = "/counter" },
-                new() { Text = "Weather", Icon = "fa-solid fa-fw fa-database", Url = "/weather" },
-                new() { Text = "Table", Icon = "fa-solid fa-fw fa-table", Url = "/table" },
-                new() { Text = "花名册", Icon = "fa-solid fa-fw fa-users", Url = "/users" }
-            };
+                Text = "返回组件库", Icon = "fa-solid fa-fw fa-home", Url = "https://www.blazor.zone/components"
+            },
+            new MenuItem
+            {
+                Text = "Index", Icon = "fa-solid fa-fw fa-flag", Url = "/", Match = NavLinkMatch.All
+            },
+            new MenuItem
+            {
+                Text = "Counter", Icon = "fa-solid fa-fw fa-check-square", Url = "/counter"
+            },
+            new MenuItem
+            {
+                Text = "Weather", Icon = "fa-solid fa-fw fa-database", Url = "/weather"
+            },
+            new MenuItem
+            {
+                Text = "Table", Icon = "fa-solid fa-fw fa-table", Url = "/table"
+            },
+            new MenuItem
+            {
+                Text = "花名册", Icon = "fa-solid fa-fw fa-users", Url = "/users"
+            }
+        };
 
-            return menus;
-        }
+        return menus;
     }
 }
