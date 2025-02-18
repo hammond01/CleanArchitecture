18c4106a20f9bafcce573d8c17c8379f7f64a57d
Hammond <hieutruonghoang01@gmail.com>
2025-02-18 16:45:50 +0700

    Update Entities and Clean Source

--- src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Shared/DateTimes/IDateTimeProvider.cs
+++ src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Shared/DateTimes/IDateTimeProvider.cs
@@ -2,13 +2,9 @@
 
 public interface IDateTimeProvider
 {
-    DateTime Now { get; }
-
-    DateTime UtcNow { get; }
-
     DateTimeOffset OffsetNow { get; }
 
     DateTimeOffset OffsetUtcNow { get; }
 
-    DateTime VietNameseTimeNow { get; }
+    DateTimeOffset VietNameseTimeNow { get; }
 }
--- src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/DateTimes/DateTimeProvider.cs
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/DateTimes/DateTimeProvider.cs
@@ -2,13 +2,8 @@
 
 public class DateTimeProvider : IDateTimeProvider
 {
-
-    public DateTime Now => DateTime.Now;
-
-    public DateTime UtcNow => DateTime.UtcNow;
-
     public DateTimeOffset OffsetNow => DateTimeOffset.Now;
 
     public DateTimeOffset OffsetUtcNow => DateTimeOffset.UtcNow;
-    public DateTime VietNameseTimeNow => TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "SE Asia Standard Time");
+    public DateTimeOffset VietNameseTimeNow => TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "SE Asia Standard Time");
 }
