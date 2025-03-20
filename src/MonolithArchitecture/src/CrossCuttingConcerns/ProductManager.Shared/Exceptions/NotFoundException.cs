b819488b2d6cbdc2dfee64615aeb597f50ef87dd
Hammond <Hieutruonghoang01@gmail.com>
2025-03-20 15:41:54 +0700

    Add Global excrption,...

---
+++ src/MonolithArchitecture/src/CrossCuttingConcerns/ProductManager.Shared/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductManager.Shared.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+    {
+    }
+
+    public NotFoundException(string message)
+        : base(message)
+    {
+    }
+}
---
+++ src/MonolithArchitecture/src/Infrastructure/ProductManager.Infrastructure/Middleware/LoggingStatusCodeMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+namespace ProductManager.Infrastructure.Middleware;
+
+public class LoggingStatusCodeMiddleware
+{
+    private readonly ILogger<LoggingStatusCodeMiddleware> _logger;
+    private readonly RequestDelegate _next;
+
+    public LoggingStatusCodeMiddleware(RequestDelegate next, ILogger<LoggingStatusCodeMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await _next(context);
+
+        var statusCode = context.Response.StatusCode;
+        var path = context.Request.Path;
+        var method = context.Request.Method;
+        var userId = (context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? context.User.FindFirst("sub")?.Value) ?? "Anonymous";
+        var remoteIp = context.Connection.RemoteIpAddress;
+
+        var statusCodes = new[]
+        {
+            StatusCodes.Status401Unauthorized, StatusCodes.Status403Forbidden
+        };
+
+        if (statusCodes.Contains(statusCode))
+        {
+            _logger.LogWarning(
+            $"StatusCode: {statusCode}, UserId: {ReplaceCRLF(userId)}, Path: {ReplaceCRLF(path)}, Method: {ReplaceCRLF(method)}, IP: {remoteIp}");
+        }
+    }
+
+    private static string ReplaceCRLF(string text) => text.Replace("\r", "\\r").Replace("\n", "\\n");
+}
