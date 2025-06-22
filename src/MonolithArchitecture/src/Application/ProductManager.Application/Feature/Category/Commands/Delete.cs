2f43a0cbc04e15abeddbbe3c1923d7e8fa944441
Hammond <hieutruonghoang01@gmail.com>
2025-06-22 21:41:27 +0700

    feat: Add explicit using directives to Category feature
    - Add explicit using directives to Category Commands (AddOrUpdate, Delete)
    - Add explicit using directives to Category Queries (GetById, Gets)
    - Add explicit using directives to Category EventHandlers (Created, Updated, Delete)
    - Include necessary imports for CategoryDto, Domain.Entities, MediatR, System namespaces
    - Complete Category feature transition from global usings to explicit imports

--- src/MonolithArchitecture/src/Application/ProductManager.Application/Feature/Category/Commands/Delete.cs
+++ src/MonolithArchitecture/src/Application/ProductManager.Application/Feature/Category/Commands/Delete.cs
@@ -1,4 +1,9 @@
-﻿namespace ProductManager.Application.Feature.Category.Commands;
+﻿using ProductManager.Application.Common.Commands;
+using ProductManager.Application.Common.Services;
+using ProductManager.Constants.ApiResponseConstants;
+using ProductManager.Domain.Common;
+using ProductManager.Domain.Entities;
+namespace ProductManager.Application.Feature.Category.Commands;
 
 public class DeleteCategoryCommand : ICommand<ApiResponse>
 {
