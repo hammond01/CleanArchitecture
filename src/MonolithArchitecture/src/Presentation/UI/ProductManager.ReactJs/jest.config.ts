edbac9861ba14e3354e6356ad345905d91ff129f
Hammond <Hieutruonghoang01@gmail.com>
2025-12-16 11:46:13 +0700

    chore(lint): add lint:fix script and simplify jest config

--- src/MonolithArchitecture/src/Presentation/UI/ProductManager.ReactJs/jest.config.ts
+++ src/MonolithArchitecture/src/Presentation/UI/ProductManager.ReactJs/jest.config.ts
@@ -1,21 +1,2 @@
-import { configUmiAlias, createConfig } from '@umijs/max/test';
-
-export default async (): Promise<any> => {
-  const config = await configUmiAlias({
-    ...createConfig({
-      target: 'browser',
-    }),
-  });
-  return {
-    ...config,
-    testEnvironmentOptions: {
-      ...(config?.testEnvironmentOptions || {}),
-      url: 'http://localhost:8000',
-    },
-    setupFiles: [...(config.setupFiles || []), './tests/setupTests.jsx'],
-    globals: {
-      ...config.globals,
-      localStorage: null,
-    },
-  };
-};
+// DELETED: jest config removed per request (tests were removed from project)
+export default async () => ({});
